2f5121424a41f72086b6de8b37153479b7356f37
Andrew <andrewdge411@gmail.com>
2021-07-19 18:23:30 -0400

    image fixed

--- components/versionButton.js
+++ components/versionButton.js
@@ -8,7 +8,6 @@ const VersionButton = (props) => {
                     {props.version}
                 </div>
             </div>
-            
         </button>
     )
     
--- pages/floor9.js
+++ pages/floor9.js
@@ -8,7 +8,7 @@ const floor9 = () => {
         <>
             
             <Layout className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-                <div className="object-cover bg-local bg-center bg-no-repeat bg-cover h-screen bg-watatsumi -z-1 px-20 py-36">
+                <div className="object-cover bg-fixed bg-center bg-no-repeat bg-cover h-screen bg-watatsumi -z-1 px-20 py-36">
                     <Content>
                     </Content>
                 </div>
