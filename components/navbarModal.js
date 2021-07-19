2318e3f240a0c4fde7ec93c5fd77ed08c759f81e
Andrew <andrewdge411@gmail.com>
2021-07-19 19:18:01 -0400

    floor 9 changes

--- components/navbarModal.js
+++ components/navbarModal.js
@@ -8,23 +8,24 @@ const NavbarModal = (props) => {
     return (
         <Transition show={props.isOpen} as={Fragment}
             appear
-            enter="transition ease-in-out duration-300 transform"
+            enter="transition ease-in-out duration-150 transform"
             enterFrom="-translate-x-full"
             enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
+            leave="transition ease-in-out duration-150 transform"
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
         >
-            {/* onMouseLeave={() =>props.setIsOpen(false)} */}
+            {/* TODO: onMouseLeave={() =>props.setIsOpen(false)} */}
             <Dialog className="absolute h-screen top-0 z-10 w-full md:w-1/3" 
                 open={props.isOpen} onClose={() => props.setIsOpen(false)} >
                 <Dialog.Overlay />
                 <div className="bg-gradient-to-b from-yellow-100 h-screen">
                     <div className='flex flex-row'>
-                        <div className='w-100 h-screen bg-gray-800' content=''>
+                        <div className='w-1/5 md:w-2/12 h-screen bg-gray-800' content=''>
                             
                         </div>
-                        <div className="p-4 w-full h-1/2">
+                        <div className='flex flex-col'>
+                            <div className="p-4 w-full h-1/2">
                                 <div className="w-full h-full">
                                     <TwitterTimelineEmbed
                                         sourceType="profile"
@@ -43,6 +44,7 @@ const NavbarModal = (props) => {
                                 <VersionButton version={"july waning phase"} />
                                 <VersionButton version={"august waxing phase"} />
                             </div>
+                        </div>
                     </div>
                 </div>
             </Dialog>
--- pages/floor9.js
+++ pages/floor9.js
@@ -8,7 +8,7 @@ const floor9 = () => {
         <>
             
             <Layout className="relative flex items-center justify-center h-full mb-12 overflow-hidden">
-                <div className="object-cover bg-fixed bg-center bg-no-repeat bg-cover min-h-screen bg-watatsumi -z-1 px-20 py-36">
+                <div className="object-cover bg-fixed bg-center bg-no-repeat bg-cover min-h-screen bg-watatsumi -z-1 md:px-20 py-36">
                     <Content>
                     </Content>
                 </div>
