a999a981830f30db19601a29bec72eb424252674
Andrew <andrewdge411@gmail.com>
2021-07-19 13:34:43 -0400

    local hosted assets

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import NavbarModal from './navbarModal';
+import Paimon from '../public/paimon.png'
 
 const Navbar = () => {
     let [isOpen, setIsOpen] = useState(false);
@@ -9,7 +10,7 @@ const Navbar = () => {
             <NavbarModal isOpen={isOpen} setIsOpen={setIsOpen}/>
             <div className='p-3'>
                 <button>
-                    <Image className='cursor-pointer z-0' src="https://spiralabyss.s3.amazonaws.com/paimon.png" alt="paimon" 
+                    <Image className='cursor-pointer z-0' src={Paimon} alt="paimon" 
                         width={100} height={116} onMouseOver={() => setIsOpen(true) } onBlur={() => setIsOpen(false)}
                     />
                     
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = () => {
     <Layout className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
       <div>
         <video className='object-cover w-screen h-screen fixed inset-0 -z-10' autoPlay loop muted>
-          <source src="https://spiralabyss.s3.amazonaws.com/SpiralAbyssMP4.mp4" type="video/mp4"></source>
+          <source src="/SpiralAbyssMP4.mp4" type="video/mp4"></source>
         </video>
       </div>
       <section className="section">
