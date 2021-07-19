f0b24b7b75cdc1972d6fb58a8f7ba0df28ef8949
Andrew <andrewdge411@gmail.com>
2021-07-18 21:10:57 -0400

    first commit

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );  
+}
+
+export default Layout;
