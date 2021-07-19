58943da4932e2853d4276edadce32b7aceddae08
Andrew <andrewdge411@gmail.com>
2021-07-19 13:54:59 -0400

    changed cache config for mp4

--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['spiralabyss.s3.amazonaws.com'],
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|mp4)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 }
