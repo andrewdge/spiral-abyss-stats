6d9e76ac282a1fe26779266a015b79d5a68a2f39
Andrew <andrewdge411@gmail.com>
2021-07-26 17:12:05 -0400

    cleaned up nav

--- components/navbarModal.js
+++ components/navbarModal.js
@@ -57,10 +57,7 @@ const NavbarModal = (props) => {
                             </div>
                             {/* data nav */}
                             <div className="flex flex-row flex-wrap gap-2 justify-start justify-items-center p-4 pt-0">
-                                <VersionButton version={"floor9"} link={"/floor9"} />
-                                <VersionButton version={"july waxing phase"} link={''}/>
-                                <VersionButton version={"july waning phase"} />
-                                <VersionButton version={"august waxing phase"} />
+                                <VersionButton version={"1.6 Floor 12"} link={"/floor12"} />
                             </div>
                             
                         </div>
