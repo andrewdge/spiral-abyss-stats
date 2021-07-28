597ac43e9b4c89cfe2c0c857ccd68239baa28767
Andrew <andrewdge411@gmail.com>
2021-07-28 17:53:18 -0400

    renamed floor12

--- components/lunarPhaseLinks.js
+++ components/lunarPhaseLinks.js
@@ -6,7 +6,7 @@ const LunarPhaseLinks = () => {
     return (
         <>
             <div className="flex flex-row flex-wrap gap-2 justify-start justify-items-center p-4 pt-0">
-                <VersionButton version={"1.6 Floor 12"} link={"/floor12"} />
+                <VersionButton version={"1.6_3 Floor 12"} link={"/1_6_3_floor12"} />
             </div>
         </>
     )
