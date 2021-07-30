c2b5f4f763e48304008cf39177f5b46318670b53
Claus-git <dmscheinberg@gmail.com>
2021-07-30 15:41:17 -0500

    added teambuilder to phaseData

--- pages/[phaseData].js
+++ pages/[phaseData].js
@@ -2,13 +2,17 @@ import React from 'react'
 import Layout from '../components/layout'
 import CompRanks from '../components/compRanks'
 import FileNames from '../data/fileNames.json'
+import TeamBuilder from '../components/teambuilder'
 
 const PhaseData = (props) => {
     return (
         <>
             <Layout>
                 <div className="flex items-center justify-center bg-fixed bg-center bg-no-repeat bg-cover bg-watatsumi -z-1 py-16 px-10  w-full min-h-screen h-full">
-                    <CompRanks data={props.file}/>
+                    <div className='flex flex-col gap-2 w-full lg:w-1/2'>
+                        <TeamBuilder/>
+                        <CompRanks data={props.file}/>
+                    </div>
                 </div>
             </Layout>
         </>
