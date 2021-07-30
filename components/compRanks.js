0bbf01533fd433c4a9f303152b8b05fa99ecb565
Claus-git <dmscheinberg@gmail.com>
2021-07-30 17:11:41 -0500

    comment out error

--- components/compRanks.js
+++ components/compRanks.js
@@ -24,7 +24,7 @@ const CompRanks = (props) => {
                 {/** speeding up loadtime with filter */}
                 {props.data
                     .filter(comp => comp.usage_rate > 1)
-                    .filter(comp => !props.filterComps || )
+                    // .filter(comp => !props.filterComps || )
                     .map((comp, index) => 
                     (index < numTeams) ? <TeamContainer key={index} c1={comp.char_one} c2={comp.char_two} c3={comp.char_three} c4={comp.char_four} rank={index+1} usage={comp.usage_rate} /> : <></>
                 )}
