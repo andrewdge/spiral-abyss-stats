81b798b2b673b310fa4a56b3499d34c5747d3678
Claus-git <dmscheinberg@gmail.com>
2021-08-02 16:56:30 -0500

    made team builder collapsable

--- components/teambuilder.js
+++ components/teambuilder.js
@@ -3,6 +3,7 @@ import Layout from './layout'
 import TeamSelection from './teamSelection'
 import { Disclosure } from '@headlessui/react'
 import { ChevronRightIcon } from '@heroicons/react/solid'
+import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core'
 import FilterSwitch from './filterSwitch'
 import dynamic from 'next/dynamic'
 
@@ -12,18 +13,15 @@ const DynamicCharacterFilter = dynamic(() => import('./characterFilter'), {
 
 const TeamBuilder = (props) => {
     return (
-        <div  className='p-4 flex flex-col bg-menu-gray border-white border-opacity-50 border-4 rounded-lg'>
-            <div className='flex justify-center text-center text-white font-bold font-serif text-xl'>
-                TEAM BUILDER
-            </div>
-            <div className='flex flex-row flex-wrap justify-center'>
+        <div className=' flex flex-col gap-2 items-center sticky top-0'>
+            <div className='flex flex-row flex-wrap justify-center items-center'>
                 <TeamSelection hero={props.chars.first} setHero={props.setChars.first} heroList={props.heroList} />
                 <TeamSelection hero={props.chars.second} setHero={props.setChars.second} heroList={props.heroList} />
                 <TeamSelection hero={props.chars.third} setHero={props.setChars.third} heroList={props.heroList} />
                 <TeamSelection hero={props.chars.fourth} setHero={props.setChars.fourth} heroList={props.heroList} />
-                <div className="flex flex-row items-center p-2 md:p-0"><FilterSwitch filter={props.filterComps} setFilter={props.setFilterComps} /></div>
             </div>
-            <div className="bg-white bg-opacity-40 rounded-lg">
+            <div className="flex flex-row items-center p-2 md:p-0"><FilterSwitch filter={props.filterComps} setFilter={props.setFilterComps} /></div>
+            <div className="bg-white bg-opacity-40 rounded-lg w-full">
                 <Disclosure>
                     {({ open }) => (
                         <>
--- pages/[phaseData].js
+++ pages/[phaseData].js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout'
 import CompRanks from '../components/compRanks'
 import FileNames from '../data/fileNames.json'
-import TeamBuilder from '../components/teambuilder'
+import TeamBuilderCollapsable from '../components/teamBuilderCollapsable'
 import { useHeroes } from '../data/typedMock'
 
 const PhaseData = (props) => {
@@ -40,10 +40,10 @@ const PhaseData = (props) => {
 
     return (
         <>
-            <Layout>
+            <Layout >
                 <div className="flex items-center justify-center bg-fixed bg-center bg-no-repeat bg-cover bg-watatsumi -z-1 py-16 px-10  w-full min-h-screen h-full">
                     <div className='flex flex-col gap-2 w-full lg:w-1/2'>
-                        <TeamBuilder 
+                        <TeamBuilderCollapsable 
                             heroList={heroList} heroDict={heroDict}
                             chars={chars} setChars={setChars}
                             checked={checked} setChecked={setChecked}
@@ -70,7 +70,7 @@ export async function getStaticProps(ctx) {
     const res = await fetch(`https://spiralabyss.s3.amazonaws.com/${ctx.params.phaseData}.json`)
     const file = await res.json()
     return {
-        props: {'file': file}
+        props: { file: file }
     }
 }
 
