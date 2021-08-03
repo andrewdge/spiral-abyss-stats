c6974f6d8b21f12aef7d93ab0b603573683a00ff
Andrew <andrewdge411@gmail.com>
2021-08-03 19:08:10 -0400

    made links work with tooltips

--- components/navButton.js
+++ components/navButton.js
@@ -19,13 +19,15 @@ const NavButton = (props) => {
             <props.icon fontSize={props.fontSize}/>
         </div>
         : 
-        <Link href={''+ props.link} passHref>
-            <Tippy content={props.content} placement='right'>
-                <button className='m-5 transition duration-100 transform hover:-translate-y-1 hover:scale-110'>
-                    <props.icon fontSize={props.fontSize} style={{ color: props.color}} />
-                </button>
-            </Tippy>
-        </Link>
+        <Tippy content={props.content} placement='right'>
+            <div>
+                <Link href={''+ props.link} passHref>
+                    <button className='m-5 transition duration-100 transform hover:-translate-y-1 hover:scale-110'>
+                        <props.icon fontSize={props.fontSize} style={{ color: props.color}} />
+                    </button>
+                </Link>
+            </div>
+        </Tippy>
     ;
 
     return (
