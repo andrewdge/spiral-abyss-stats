89228f424531e13cc7c052de30f2e21a43bb7c12
Andrew <andrewdge411@gmail.com>
2021-11-15 18:35:28 -0500

    possible head fix

--- components/layout.js
+++ components/layout.js
@@ -8,9 +8,6 @@ import Navbar from './navbar'
 const Layout = ({ children, twitter }) => {
     return (
         <>
-            <Head>
-                <meta name="description" content="Data on the most recent iteration of Genshin Impact's Spiral Abyss" />
-            </Head>
             <div>
                 <Navbar className="fixed" twitter={twitter} />
                 {children}
