45c04ee6a55cb754809f6f4e3462dc6992c92ad6
Andrew <andrewdge411@gmail.com>
2023-02-01 00:53:36 -0500

    add alhaitham

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import '../styles/globals.css'
 import Script from "next/script";
-import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <Script 
-            id="Adsense-id" onError={(e) => { console.error("Script failed to load", e);}}
-            data-ad-client={`${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`} async strategy="afterInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" crossOrigin="anonymous"
-        >
-        </Script>
-      </Head>
+      <Script 
+          id="Adsense-id" onError={(e) => { console.error("Script failed to load", e);}}
+          data-ad-client={`${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`} async strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" crossOrigin="anonymous"
+      >
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
